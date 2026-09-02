fc791c41a5353956e8fd08a49293b0a13b828ac3
phahadahosena57-spec <phahadahosena57@gmail.com>
2026-09-02 23:06:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll('a[href="#"]').forEach(a=>a.addEventListener("click",e=>e.preventDefault()));console.log("AHEAD VIA SKILL HUB loaded successfully.");});
